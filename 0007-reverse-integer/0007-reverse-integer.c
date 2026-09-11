e7b2d893ab0b18934ee6cf61fec70e77315c9d7b
DeveshPandey493 <deveshpandey493@gmail.com>
2026-09-11 11:36:32 +0530

    Time: 6 ms (22.46%), Space: 9.3 MB (9.03%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.c
@@ -0,0 +1,12 @@
+int reverse(int x){
+    long long rev = 0 , rem;
+    while(x != 0){
+        rem = x%10;
+        rev = rev * 10 + rem;
+        x = x / 10;
+    
+    if(rev > INT_MAX || rev < INT_MIN)
+        return 0;
+    }
+    return (int)rev;
+}
